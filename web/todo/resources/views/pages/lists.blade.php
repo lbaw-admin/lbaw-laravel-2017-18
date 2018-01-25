4faf20e6bb98322b5b5fb7b29c0a28dd07406d51
Andre Restivo <andre.restivo@gmail.com>
2018-01-25 14:18:38 +0000

    Responsive lists

--- web/todo/resources/views/pages/lists.blade.php
+++ web/todo/resources/views/pages/lists.blade.php
@@ -5,16 +5,16 @@
 @section('content')
 
 <section id="lists">
-@foreach ($lists as $list)
-  <article class="list">
-  <header>
-    <h2><a href="list/{{ $list->id }}">{{ $list->name }}</a></h2>
-  </header>
-  <ul>
-    @each('partials.item', $list->items, 'item')
-  </ul>
-  </article>
-@endforeach
+  @foreach ($lists as $list)
+    <article class="list">
+    <header>
+      <h2><a href="list/{{ $list->id }}">{{ $list->name }}</a></h2>
+    </header>
+    <ul>
+      @each('partials.item', $list->items, 'item')
+    </ul>
+    </article>
+  @endforeach
 </section>
 
 @endsection
