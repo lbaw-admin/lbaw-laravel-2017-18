f15ebefd8d9b9a3ae4520787b55ea482cdcedee7
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2018-01-26 15:55:16 +0000

    initial docker file and docker upload script

---
+++ upload_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOCKER_USERNAME=TODO # Replace by your docker hub username
+IMAGE_NAME=lbaw-t1-g01
+
+docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
+docker push $DOCKER_USERNAME/$IMAGE_NAME
